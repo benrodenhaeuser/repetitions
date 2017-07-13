9555e5d3f5cbeb959c1f2773db3db517d01e3051
Ben Rodenhäuser <ben@karo.berlin>
2017-07-13 22:19:17 +0200

    small tests

---
+++ small_problems/1001.rb
@@ -0,0 +1,13 @@
+# Write a method that rotates an array by moving the first element to the end of the array. The original array should not be modified.
+
+def rotate_array(array)
+  array[1..-1] + [array[0]]
+end
+
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+p x == [1, 2, 3, 4]                 # => true
--- small_problems/test.rb
+++ small_problems/test.rb
@@ -1,5 +1,22 @@
-numbers = [1,2].cycle(1)
+string = 'ben'
+p string.chars.map { |char| char * 2 }.join == 'bbeenn'
+p string.each_char.map { |char| char * 2 }.join == 'bbeenn'
 
-loop do
-  p numbers.next
+e = Enumerator.new do |y|
+  y << 1
+  y << 2
+  y << 3
 end
+
+p e
+
+
+names = %w[ben julia tom]
+e = names.enum_for(:select)
+p e
+
+selection = e.each do |name|
+  name.start_with?('b')
+end
+
+p selection
